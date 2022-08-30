7a0bd11416ac1d066f1e44d6befeb6f677c0b9bf
Harshwardhan <harshafartale12@gmail.com>
2022-08-30 16:06:58 +0530

    Completed

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import OutputWindow from "./OutputWindow";
+import CustomInput from "./CustomInput";
+import OutputDetails from "./OutputDetails";
+import ThemeDropdown from "./ThemeDropdown";
+import CodeEditorWindow from "./CodeEditorWindow";
+import LanguagesDropdown from "./LanguagesDropdown";
+import Footer from "./Footer";
+import useKeyPress from "../hooks/useKeyPress";
+
+export {
+  OutputWindow,
+  CustomInput,
+  OutputDetails,
+  ThemeDropdown,
+  LanguagesDropdown,
+  useKeyPress,
+  Footer,
+  CodeEditorWindow,
+};
