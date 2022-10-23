2e62f1e9cc0bb8f2b14f101aacf4b7e13d9c4109
Harshwardhan <harshafartale12@gmail.com>
2022-10-23 12:28:00 +0530

    Changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 // import CodeEditorWindow from "./CodeEditorWindow";
 import axios from "axios";
